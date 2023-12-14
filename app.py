1379404981fc2c6ce30af4adb7cbba37438a8759
RubyChen <rubychen@ntu.im>
2023-12-15 03:18:46 +0800

    fastapi to flask

--- app.py
+++ app.py
@@ -35,6 +35,24 @@ conversation = ConversationChain(
     memory=memory,
     verbose=False
 )
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
  
 
 @handler.add(MessageEvent, message=TextMessage)
