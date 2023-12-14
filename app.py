14c243d90e761548069289845828436e78b47f62
RubyChen <rubychen@ntu.im>
2023-12-15 02:21:13 +0800

    try openai

--- app.py
+++ app.py
@@ -1,48 +1,87 @@
-from flask import Flask, request, abort
+import os
+import sys
+
+import aiohttp
+
+from fastapi import Request, FastAPI, HTTPException
+
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferWindowMemory
+
 
 from linebot import (
-    LineBotApi, WebhookHandler
+    AsyncLineBotApi
 )
+from linebot.v3.webhook import WebhookParser
+
+from linebot.aiohttp_async_http_client import AiohttpAsyncHttpClient
 from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-
 import os
-from model import generate_text
+import openai
 
-app = Flask(__name__)
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())  # read local .env file
 
-line_bot_api = LineBotApi(os.getenv("CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.getenv("CHANNEL_SECRET"))
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('CHANNEL_SECRET', None)
+channel_access_token = os.getenv('CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
+app = FastAPI()
+session = aiohttp.ClientSession()
+async_http_client = AiohttpAsyncHttpClient(session)
+line_bot_api = AsyncLineBotApi(channel_access_token, async_http_client)
+parser = WebhookParser(channel_secret)
+
+# Langchain 串接 OpenAI ，這裡 model 可以先選 gpt-3.5-turbo
+llm = ChatOpenAI(temperature=0, model='gpt-3.5-turbo')
+
+# 透過 ConversationBufferWindowMemory 快速打造一個具有「記憶力」的聊天機器人，可以記住至少五回。
+# 通常來說 5 回還蠻夠的
+memory = ConversationBufferWindowMemory(k=5)
+conversation = ConversationChain(
+    llm=llm,
+    memory=memory,
+    verbose=False
+)
+
+
+@app.post("/callback")
+async def handle_callback(request: Request):
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    body = await request.body()
+    body = body.decode()
 
-    # handle webhook body
     try:
-        handler.handle(body, signature)
+        events = parser.parse(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+        raise HTTPException(status_code=400, detail="Invalid signature")
+
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+
+        # 將使用者傳來的訊息 event.message.text 當成輸入，等 LangChain 傳回結果。
+        ret = conversation.predict(input=event.message.text)
+
+        await line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=ret)
+        )
 
     return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=generate_text(event.message.text)))
-
-
-if __name__ == "__main__":
-    app.run()
