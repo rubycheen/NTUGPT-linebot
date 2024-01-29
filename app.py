72b34747de588dce08423d70390891da84399f23
RubyChen <rubychen@ntu.im>
2024-01-29 15:29:42 +0800

    RAG6

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         # TextSendMessage(text=))
-        TextSendMessage(text=result))
+        TextSendMessage(text=result.text))
 
 
 if __name__ == "__main__":
