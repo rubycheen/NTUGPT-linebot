0ccf52e7d144bd6010ee7ed2e6a584d73b27677a
RubyChen <rubychen@ntu.im>
2024-01-29 14:38:38 +0800

    RAG int

--- app.py
+++ app.py
@@ -15,26 +15,28 @@ from langchain.memory import ConversationBufferWindowMemory
 
 import os
 import openai
+import requests
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())  # read local .env file
 
 app = Flask(__name__)
-
+url = 'https://dcde-140-112-90-16.ngrok-free.app/predict'
 line_bot_api = LineBotApi(os.getenv("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.getenv("CHANNEL_SECRET"))
 
 # Langchain 串接 OpenAI ，這裡 model 可以先選 gpt-3.5-turbo
-llm = ChatOpenAI(temperature=0, model='gpt-3.5-turbo')
+# llm = ChatOpenAI(temperature=0, model='gpt-3.5-turbo')
 
-# 透過 ConversationBufferWindowMemory 快速打造一個具有「記憶力」的聊天機器人，可以記住至少五回。
-# 通常來說 5 回還蠻夠的
-memory = ConversationBufferWindowMemory(k=5)
-conversation = ConversationChain(
-    llm=llm,
-    memory=memory,
-    verbose=False
-)
+# # 透過 ConversationBufferWindowMemory 快速打造一個具有「記憶力」的聊天機器人，可以記住至少五回。
+# # 通常來說 5 回還蠻夠的
+# memory = ConversationBufferWindowMemory(k=5)
+# conversation = ConversationChain(
+#     llm=llm,
+#     memory=memory,
+#     verbose=False
+# )
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -57,10 +59,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    result = conversation.predict(input=event.message.text)  # 使用模型實例進行預測
+    data = {'prompt': event.message.text}
+    result = requests.post(url, json=data)
+    print(result)
+    # result = conversation.predict(input=event.message.text)  # 使用模型實例進行預測
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=result))
+        TextSendMessage(text=f"{result['answer']}\n參考資料：\n{result['urls']}"))
 
 
 if __name__ == "__main__":
